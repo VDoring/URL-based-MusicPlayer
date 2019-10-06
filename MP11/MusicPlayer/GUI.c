1bd3c5b9734d6c11f45b49f8cb3b68deaecd640f
VDoring <hojun.dev@gmail.com>
2019-10-06 11:49:19 +0900

    Ver 11, separation of regeneration function after first and second

---
+++ MP11/MusicPlayer/GUI.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+
+int main_input; //메인화면에서 메뉴 선택시 사용
+int play_input; //곡 재생모드 선택시 사용
+int play_continue_input; //계속 재생할지 여부(숫자)를 담는 변수
+
+gotoxy(int x, int y) {
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+
+void mainTheme() { //실행시 화면 구성
+	system("mode con cols=40 lines=13");
+	int x = 0, y = 0;
+	gotoxy(x + 8, y + 1); printf("[ [ VDoring's Player ] ]");
+	gotoxy(x + 15, y + 4); printf("[1] Play");
+	gotoxy(x + 15, y + 6); printf("[2] Exit");
+	gotoxy(x, y + 8); printf("________________________________________");
+	gotoxy(x + 10, y + 10); printf("Input : ");
+	gotoxy(x + 19, y + 10); scanf_s("%d", &main_input);
+}
+
+void playmode() {
+	system("mode con cols=40 lines=13");
+	int x = 0, y = 0;
+	gotoxy(x + 11, y + 1); printf("[ [ Play Mode ] ]");
+	gotoxy(x, y + 3); printf("1. from top to bottom");
+	gotoxy(x, y + 5); printf("2. from bottom to top");
+	gotoxy(x, y + 7); printf("3. random");
+	gotoxy(x, y + 9); printf("________________________________________");
+	gotoxy(x + 10, y + 11); printf("Input : ");
+	gotoxy(x + 19, y + 11); scanf_s("%d", &play_input);
+}
+
+void Question_Continue() {
+	system("mode con cols=40 lines=13");
+	int x = 0, y = 0;
+	gotoxy(x, y + 5); printf("Nwxt Music Continue?");
+	gotoxy(x, y + 7); printf("[1] Yes     [2] No");
+	gotoxy(x + 10, y + 11); printf("Input : ");
+	gotoxy(x + 19, y + 11); scanf_s("%d", &play_continue_input);
+}
---
+++ MP11/MusicPlayer/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include "head.h"
+
+int main() {
+
+	while (1) {
+		mainTheme();
+		if (main_input == 1) {
+			playmode();
+			if (play_input == 1) {
+				Musiclist_line_Read();
+				Musiclist_FirstPlay();
+				Question_Continue();
+				if (play_continue_input == 1) {
+					Musiclist_ContinuePlay();
+				}
+				else if (play_continue_input == 2) {
+					return 5;
+				}
+				else return 10;
+			}
+			else if (play_input == 2) {
+				printf("2번 개발중");
+				return 2;
+			}
+			else if (play_input == 3) {
+				printf("3번 개발중");
+				return 2;
+			}
+		}
+
+		else if (main_input == 2) {
+			if (fp != NULL) { //(음악)파일이 열어져 있을때
+				File_close();
+			}
+			return 1;
+		}
+
+		else {
+			printf("Please re-enter!"); Sleep(300);
+		}
+	}
+}
