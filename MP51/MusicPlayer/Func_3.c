506b4805d920e887e6eaf1facc6154ff15c47bf2
VDoring <hojun.dev@gmail.com>
2020-01-10 22:57:59 +0900

    Ver 51, (temporary)Respond to 3-2 func empty line

---
+++ MP51/MusicPlayer/Func_3.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <string.h>
+
+FILE *fp; // .txt파일 전용 파일포인터
+
+int *line_num; // .txt파일 라인 수 임시저장
+int line_number; // .txt파일 라인 수 저장
+void Musiclist_line_Read_3() { // .txt파일 라인 수 카운팅
+
+	fopen_s(&fp, "Mlist.txt", "rt");
+	int line_count = 0;
+	char tmp;
+
+	while (fscanf_s(fp, "%c", &tmp, sizeof(tmp)) != EOF) { // .txt파일에 저장된 글자 하나하나 검사해 EOF인지 확인
+		if (tmp == '\n')
+			line_count++; // 파일 줄 수 최초 저장
+	}
+	line_num = &line_count;
+	line_number = *line_num; // .txt파일 라인 수 최종 저장
+
+	fclose(fp);
+
+}
+
+
+int random_number; //rand함수의 값을 담는 곳
+int linkRandom_line_number = 0; // .txt파일 라인 수 저장
+int Random; //random_number의 값을 받는 변수(랜덤숫자 최종)
+void Random_Select() { //난수 기반으로 재생할 링크를 정하는 함수
+	linkRandom_line_number = line_number; //line_number는 main.c에서도 사용하므로 혼동 생기지않게 똑같은 값을 가진 변수를 따로선언
+	linkRandom_line_number += 2;
+	Random = 0;
+	
+	while (Random <= 0) { // Random이 1 이상이 될때까지 반복
+		srand((unsigned)time(NULL)); //프로그램 실행시마다 다른 랜덤숫자가 나오게 된다
+		random_number = rand();
+		Random = (int)random_number % linkRandom_line_number; // Random은 난수와 라인 수+2의 나머지다
+	}
+}
+
+int empty_line_check = 0; //빈라인 체크를 했는지 확인하는 변수. 0과 1로 구분
+char fileread[4096]; // .txt파일 한줄읽기용 문자열
+//char save_empty_line_number[8000] = { 0, }; //빈 라인의 번호를 저장하는 배열. Random변수의 값을 저장함
+const char CMD_Static_command_3[100] = { "start chrome --incognito" }; // 크롬 시크릿모드 실행 명령어
+char cache_Music1_3[100];// 명령어 저장
+char cache_Music2_3[8092];// fileread 문자열과 같은 역할
+char *ptr_linkcut_result; // 오직 링크만 저장
+char *contact_3 = NULL; //cache_Music2_2 문자열에서 자른 나머지 문자열을 저장
+char Musiclink_3[8192] = { 0, }; // 최종 음악재생 명령어
+void Musiclist_Play() {
+	fopen_s(&fp, "Mlist.txt", "rt");
+	rewind(fp);
+
+
+	//실행 전에 먼저 빈 라인을 체크하는 구문. 한번만 실행되어야 함
+	if (empty_line_check == 0) {
+		empty_line_check++; //한번만 실행하기 위한 구분
+		int empty_line_check_for = line_number + 1; //밑의 for문에 사용할 변수
+		int empty_line_count_minus = 0; //빈 라인 갯수 카운트
+
+		for (int i = 0; i < empty_line_check_for; i++) {//총 줄 수만큼 반복
+			fgets(fileread, sizeof(fileread), fp);
+			if (fileread[0] == '\n') empty_line_count_minus++; //공백이면 빈라인 카운트 + 1
+			else continue;
+		}
+		line_number = line_number - empty_line_count_minus; //계산 완료 후 빈라인의 수를 뺀다.
+		rewind(fp);
+	}
+
+	for (int i = 0; i < Random; i++) {
+		fgets(fileread, sizeof(fileread), fp);
+	}
+
+	//랜덤값으로 뽑은 라인의 공백 여부 채크 및 다시 뽑기
+	//int empty_line_for = 0;
+	if (fileread[0] == '\n') { //만약 공백인 경우
+		while (1) {
+			rewind(fp);
+			Random_Select(); //랜덤숫자를 다시 고름
+			for (int j = 0; j < Random; j++) {
+				fgets(fileread, sizeof(fileread), fp);
+			}
+			if (fileread[0] == '\n') continue; //그래도 공백일 경우 똑같은 과정을 반복
+			else if (fileread[0] != '\n') break; //링크를 찾았을 경우 루프 탈출
+		}
+	}
+	/*
+	빈 라인 번호를 받을 배열 선언
+	만약 빈 라인일 경우 그 배열에 저장.
+	만약 Random값이 그 배열에 있을경우 취소, 다른걸 재생한다.
+
+	...라고 계획을 세웠으나, 이 기능은 중복 곡 재생이 가능했기에 필요가 없었다.
+	그냥 공백여부 확인과 다시 뽑는것으로 방법을 바꾸겠다.
+	*/
+
+
+	sprintf_s(cache_Music1_3, sizeof(cache_Music1_3), "%s", CMD_Static_command_3);
+	sprintf_s(cache_Music2_3, sizeof(cache_Music2_3), "%s", fileread);
+
+	ptr_linkcut_result = strtok_s(cache_Music2_3, " ", &contact_3); // 한줄 읽은 내용을 띄어쓰기 기준으로 나누어 ptr_linkcut_result에 저장
+
+	sprintf_s(Musiclink_3, sizeof(Musiclink_3), "%s %s", cache_Music1_3, ptr_linkcut_result); // 최종 음악재생 명령어
+
+	system(Musiclink_3);
+
+	fclose(fp);
+
+}
+
+void File_Close3() {
+	empty_line_check = 0;
+	fclose(fp);
+}
---
+++ MP51/MusicPlayer/Func_4.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <string.h>
+
+#define MAX_COUNT line_number + 1
+#define MIXING_COUNT line_number + 31
+
+FILE *fp; // .txt파일 전용 파일포인터
+
+
+int line_number;
+int musiclist_arr[10000] = { 0, };
+void No_overlap_random() { //순서대로 나열된 배열을 셔플 및 배열에 저장
+	int i, swap_temp, x, y, z1, z2;
+
+	srand((unsigned int)time(NULL));
+
+	for (i = 0; i < MAX_COUNT; i++) musiclist_arr[i] = i + 1; //1 ~ 라인 수만큼 배열에 수 나열
+
+	for (i = 0; i < MIXING_COUNT; i++) { //배열에 나열된 수를 셔플한다.
+		x = rand() % MAX_COUNT; //랜덤수 x
+		y = rand() % MAX_COUNT; //랜덤수 y
+		if (x != y) {
+			swap_temp = musiclist_arr[x];
+			musiclist_arr[x] = musiclist_arr[y];
+			musiclist_arr[y] = swap_temp;
+		}
+	}
+	//(위의 코드만으론 musiclist_arr[0]배열의 값이 섞이지 않는 문제가 있어서 따로 코드를 적는다.
+	z1 = rand() % MAX_COUNT; //랜덤수 z1
+	z2 = rand() % MAX_COUNT; //랜덤수 z2
+	swap_temp = musiclist_arr[0];
+	musiclist_arr[0] = musiclist_arr[z1];
+	musiclist_arr[z1] = swap_temp;
+
+	//(바로 위의 코드만 적용하면 txt파일 첫 라인의 링크가 
+	swap_temp = musiclist_arr[z1];
+	musiclist_arr[z1] = musiclist_arr[z2];
+	musiclist_arr[z2] = swap_temp;
+}
+
+/*
+[이후 빈라인 대응 방향]
+
+먼저 빈 라인인지 체크
+빈라인일경우 해당 빈라인의 musiclist_arr 숫자 값을 0으로 만듬
+
+이후 musiclist_arr의 숫자를 읽을때 0이라면 그냥 다음걸로 재생되게 하고, line_number값도 하나 마이너스함
+*/
+
+
+
+int arraynum_read = 0; //save_random_number 배열의 값을 읽기위한 숫자를 저장
+char fileread[4096]; // .txt파일 한줄읽기용 문자열
+int txt_empty_lines_number_save[10000] = { 0, }; //빈라인의 배열 블럭 번호를 저장
+int txt_empty_lines_number_save_loop_number = 0; //txt_empty_lines_number_save 배열 전용 크기
+const char CMD_Static_command_4[100] = { "start chrome --incognito" }; // 크롬 시크릿모드 실행 명령어
+char cache_Music1_4[100];
+char cache_Music2_4[8092];
+char *ptr_linkcut_result;
+char *contact_4 = NULL; //cache_Music2_2 문자열에서 자른 나머지 문자열을 저장
+char Musiclink_4[8192] = { 0, }; // 최종 음악재생 명령어
+void No_overlap_Musiclist_play() { //(음악중복재생 방지) 음악플레이
+	fopen_s(&fp, "Mlist.txt", "rt");
+	rewind(fp);
+	
+	while (1) {
+		for (int j = 0; j < musiclist_arr[arraynum_read]; j++) {
+			fgets(fileread, sizeof(fileread), fp);
+		}
+
+		if (fileread[0] == '\n') {
+			arraynum_read++;
+			line_number--;
+			rewind(fp);
+		}
+		else if (fileread[0] != '\n') break;
+	}
+
+	sprintf_s(cache_Music1_4, sizeof(cache_Music1_4), "%s", CMD_Static_command_4);
+	sprintf_s(cache_Music2_4, sizeof(cache_Music2_4), "%s", fileread);
+
+	ptr_linkcut_result = strtok_s(cache_Music2_4, " ", &contact_4); // 한줄 읽은 내용을 띄어쓰기 기준으로 나누어 ptr_linkcut_result에 저장
+
+	sprintf_s(Musiclink_4, sizeof(Musiclink_4), "%s %s", cache_Music1_4, ptr_linkcut_result); // 최종 음악재생 명령어
+
+	system(Musiclink_4);
+
+	arraynum_read++;
+
+	fclose(fp);
+}
+
+
+void File_close4() { // .txt 파일 닫음
+	arraynum_read = 0;
+
+	for (int i = 0; i < 10000; i++) {
+		musiclist_arr[i] = 0;
+	}
+
+	fclose(fp);
+}
